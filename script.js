91b5cbc2662db6c072521682ab30200e6ca30b5b
DomenicoLucarelli <domyluca@gmail.com>
2023-02-28 19:21:55 +0100

    bonus 2 fatto

--- script.js
+++ script.js
@@ -14,41 +14,82 @@ let imageEl = document.getElementById('image');
 
 let index = 0
 
+let thumbnailsEl = document.getElementById('thumbnails')
+
+for (let i=0; i<imagesList.length; i++){
+
+    let newEl = document.createElement('img');
+    newEl.src=(imagesList[i]);
+    newEl.classList.add('thumbStyle');
+    thumbnailsEl.append(newEl)
+}
+
+
+let allImages = document.querySelectorAll('.thumbStyle')
+
 imageEl.src = (imagesList[index]);
 
+allImages[index].classList.add('active')
+
 downArrowEl.addEventListener('click', function(){
+    index++;
 
-    if (index < imagesList.length - 1){
+    if (index <= imagesList.length - 1){
 
-        index++;
     
         imageEl.src = (imagesList.at(index));
+
+        
+        allImages[index].classList.add('active')
+
+        allImages[index - 1].classList.remove('active')
+        
+
+
 
     } else {
         index = 0;
 
         imageEl.src = (imagesList.at(index));
 
+        allImages[index].classList.add('active')
+
+        allImages[allImages.length - 1].classList.remove('active')
+
+        
+
     }
 
 })
 
 upArrowEl.addEventListener('click', function(){
-
-    if (index > -imagesList.length ){
-
-        index--;
     
+    index--;
+
+    if (index < 0){
+        
+        index = allImages.length - 1
+
         imageEl.src = (imagesList.at(index));
+
+        allImages[index].classList.add('active')
+
+        allImages[allImages.length -1 - index].classList.remove('active')
+
+        
 
     } else {
-        index = -1;
+        
 
         imageEl.src = (imagesList.at(index));
+
+        allImages[index].classList.add('active')
+
+        allImages[index + 1].classList.remove('active')
+
+        
 
     }
 
 })
-
-
 
