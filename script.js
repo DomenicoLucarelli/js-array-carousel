a1a74bdd9cb3e19796fb7a5a9c717f16c86bb3bc
DomenicoLucarelli <domyluca@gmail.com>
2023-02-28 15:18:03 +0100

    Milestone3 + bonus1

---
+++ script.js
@@ -0,0 +1,54 @@
+/* 
+
+
+
+*/
+let upArrowEl = document.getElementById('upArrow');
+
+let downArrowEl = document.getElementById('downArrow');
+
+
+let imagesList = ['img/01.webp' , 'img/02.webp' , 'img/03.webp' , 'img/04.webp' , 'img/05.webp' ];
+
+let imageEl = document.getElementById('image');
+
+let index = 0
+
+imageEl.src = (imagesList[index]);
+
+downArrowEl.addEventListener('click', function(){
+
+    if (index < imagesList.length - 1){
+
+        index++;
+    
+        imageEl.src = (imagesList[index]);
+
+    } else {
+        index = 0;
+
+        imageEl.src = (imagesList[index]);
+
+    }
+
+})
+
+upArrowEl.addEventListener('click', function(){
+
+    if (index > -imagesList.length ){
+
+        index--;
+    
+        imageEl.src = (imagesList.at(index));
+
+    } else {
+        index = -1;
+
+        imageEl.src = (imagesList.at(index));
+
+    }
+
+})
+
+
+
